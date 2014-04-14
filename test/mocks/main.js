6a35ee84f7eb35bcbf985c5ebf29afca6a132714
Dennis Alund <dennis@oddbit.co.id>
2014-04-14 13:18:49 +0800

    Removing net mock. It's outdated.

--- test/mocks/main.js
+++
@@ -1,8 +0,0 @@
-define(function (require) {
-        "use strict";
-
-        return {
-            net: require("./net-mock")
-        };
-    }
-);
--- test/testConfig.js
+++ test/testConfig.js
@@ -4,7 +4,6 @@ define(function () {
     return {
         baseUrl: "../src",
         paths: {
-            net: "../test/mocks/net-mock",
             sax: "../lib/sax/lib/sax"
         },
         shim: {
@@ -14,7 +13,6 @@ define(function () {
             "models", "soap", "ssdp",
             {name: "unitTests", location: "../test/unit"},
             {name: "integrationTests", location: "../test/integration"},
-            {name: "mocks", location: "../test/mocks"},
             {name: "fixtures", location: "../test/fixtures"}
         ]
     };
