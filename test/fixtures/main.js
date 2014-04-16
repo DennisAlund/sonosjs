73d7622c396a10a9e47373e8cd89fb9904c2d217
Dennis Alund <dennis@oddbit.co.id>
2014-04-16 11:46:11 +0800

    Adding sample XML for media info Play state for radio and music file (it is the response to the GetPositionInfo SOAP request)

--- test/fixtures/main.js
+++ test/fixtures/main.js
@@ -28,6 +28,16 @@ define(function (require) {
                     play1: require("text!./xml/deviceDetails.play1.xml"),
                     play5: require("text!./xml/deviceDetails.play5.xml")
                 },
+                mediaInfo: {
+                    radio: {
+                        playing: require("text!./xml/mediaInfo.radio.playing.xml"),
+                        paused: require("text!./xml/mediaInfo.radio.paused.xml")
+                    },
+                    musicFile: {
+                        playing: require("text!./xml/mediaInfo.musicFile.playing.xml"),
+                        paused: require("text!./xml/mediaInfo.musicFile.paused.xml")
+                    }
+                },
                 notify: {
                     groupVolume: require("text!./xml/groupVolume.xml"),
                     lastChange: require("text!./xml/lastChange.xml")
